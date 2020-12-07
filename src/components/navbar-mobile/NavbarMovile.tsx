d2c1ed8220d3fdb800c00e28df2de4ad537f6a41
Johan Castillo <jcjohan2707@gmail.com>
2020-12-07 14:43:02 -0400

    Structure of the navbar

--- src/components/navbar-mobile/NavbarMovile.tsx
+++ src/components/navbar-mobile/NavbarMovile.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './NavbarMobile.scss';
+import logo from './logo.svg';
 
-const NavbarMobile = () => (
-    <nav>
-        <div>Item 1</div>
-        <div>Item 3</div>
-    </nav>
+const NavbarMobile = (props:any) => (
+    <div className="ed-container bg-navbar">
+        <div className="ed-item s-50 s-center">
+            <img src={logo} alt=""/>
+        </div>
+
+        <div className="ed-item s-50 s-right s-center">
+            <span className="icon">1</span>
+            <span className="icon">2</span>
+            <span className="icon">3</span>
+        </div>
+    </div>
+    
 );
 
 export default NavbarMobile;
