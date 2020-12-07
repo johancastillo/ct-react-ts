eb46cc4a9cd8625026adc57d1502bee646e6c947
Johan Castillo <jcjohan2707@gmail.com>
2020-12-07 11:30:08 -0400

    Card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import React from 'react';
 import './App.scss';
 import CardProduct from './components/card-product/CardProduct';
 
-function App() {
+function App(props:any) {
   return (
     <>
-      <CardProduct />
+      <div className="ed-grid m-grid-4 s-grid-2">
+        <CardProduct 
+          image="https://image-galery.herokuapp.com/static/media/1.691fd698.webp"
+        />
+
+        <CardProduct 
+          image="https://image-galery.herokuapp.com/static/media/1.691fd698.webp"
+        />
+
+        <CardProduct 
+          image="https://image-galery.herokuapp.com/static/media/1.691fd698.webp"
+        />
+
+        <CardProduct 
+          image="https://image-galery.herokuapp.com/static/media/1.691fd698.webp"
+        />
+
+      </div>
     </>
   );
 }
--- src/components/card-product/CardProduct.tsx
+++ src/components/card-product/CardProduct.tsx
@@ -1,27 +1,16 @@
 import React from 'react';
+import './CardProduct.scss';
 
-const CardProduct = () => (
+
+const CardProduct = (props:any) => (
     <article className="card">
         <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="path/img" alt="" />
+            <img src={props.image} alt="" />
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 className="t5 s-mb-2 s-center">
             Programación orientada a objetos con Go
             </h3>
-            <div className="s-mb-2 s-main-center">
-            <div className="card__teacher s-cross-center">
-                <div className="card__avatar s-mr-1">
-                <div className="circle img-container">
-                    <img src="path/img" alt="" />
-                </div>
-                </div>
-                <span className="small">Alexys Lozada</span>
-            </div>
-            </div>
-            <div className="s-main-center">
-            <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
-            </div>
         </div>
     </article>
 );
